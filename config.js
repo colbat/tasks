d051baf93d183a6e8229fc65297c9f539875acbc
Gaetan <gaetan_covelli@hotmail.com>
2015-11-03 14:45:16 +0100

    Supports local configuration file, keeping the config out of the repository.

--- config.js
+++ config.js
@@ -1,9 +1,16 @@
+var localConfig;
+try {
+	localConfig = require('./config_local');	
+} catch(err) {
+	localConfig = false;
+}
+
 module.exports = {
-  SUPER_USER_API_KEY: process.env.API_KEY || 'Pa$$w0rd',
-  MONGO_URI: process.env.MONGOLAB_URI || 'mongodb://localhost/tasks',
-  FACEBOOK_SECRET: process.env.FACEBOOK_SECRET || '36dbcc90d1b67e6cf33b7b9e8597c520',
-  TWITTER_KEY: process.env.TWITTER_KEY || '15klJNAfjHmJfzJsEtS9Tbd5j',
-  TWITTER_SECRET: process.env.TWITTER_SECRET || '0tZI3dSgYxfdvTzbhUdNkZGFI201pcWRIrm8VTalnKsmhkXE1M',
-  TWITTER_CALLBACK: process.env.TWITTER_CALLBACK || 'http://localhost:3000',
-  tokenSecret: process.env.tokenSecret || 'I love managing tasks, feed me more!'
+  SUPER_USER_API_KEY: process.env.API_KEY || localConfig.SUPER_USER_API_KEY,
+  MONGO_URI: process.env.MONGOLAB_URI || localConfig.MONGO_URI,
+  FACEBOOK_SECRET: process.env.FACEBOOK_SECRET || localConfig.FACEBOOK_SECRET,
+  TWITTER_KEY: process.env.TWITTER_KEY || localConfig.TWITTER_KEY,
+  TWITTER_SECRET: process.env.TWITTER_SECRET || localConfig.TWITTER_SECRET,
+  TWITTER_CALLBACK: process.env.TWITTER_CALLBACK || localConfig.TWITTER_CALLBACK,
+  tokenSecret: process.env.tokenSecret || localConfig.tokenSecret
 };
