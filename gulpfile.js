736ac1a06542f615ddd9f884491407ebacda6751
Gaetan <gaetan_covelli@hotmail.com>
2015-11-04 18:16:07 +0100

    Adds gulp

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+var gulp = require('gulp');
+var csso = require('gulp-csso');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var header = require('gulp-header');
+var complexity = require('gulp-complexity');
+var ngAnnotate = require('gulp-ng-annotate');
+
+var banner = ['/**',
+    ' * Tasks',
+    ' * (c) 2015 Gaëtan Covelli',
+    ' * Last Updated: <%= new Date().toUTCString() %>',
+    ' */',
+    ''].join('\n');
+
+gulp.task('minify', function() {
+    return gulp.src([
+        'public/vendor/jquery.min.js',
+        'public/vendor/bootstrap.min.js',
+        'public/vendor/angular.min.js',
+        'public/vendor/angular-animate.min.js',
+        'public/vendor/angular-route.min.js',
+        'public/vendor/angular-messages.min.js',
+        'public/vendor/toaster.min.js',
+        'public/vendor/satellizer.min.js',
+        'public/vendor/md5.min.js',
+        'public/app.js'
+    ])
+        .pipe(concat('app.min.js'))
+        .pipe(ngAnnotate())
+        .pipe(uglify())
+        .pipe(header(banner))
+        .pipe(gulp.dest('public'));
+});
+
+gulp.task('complexity', function() {
+    return gulp.src([
+        'public/app.js'
+    ])
+        .pipe(complexity());
+});
+
+gulp.task('styles', function() {
+  gulp.src([
+    'public/css/vendor/*.css',
+    'public/css/style.css'
+  ])
+    .pipe(concat('style.min.css'))
+    .pipe(csso())
+    .pipe(gulp.dest('public/css'));
+});
